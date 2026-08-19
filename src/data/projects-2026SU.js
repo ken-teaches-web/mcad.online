fcb492d01faefe091a1def0b232b7297ea8655af
Lafe Smith <lsmith257@mcad.edu>
2026-08-19 13:55:01 -0500

    Add files via upload
    Add Summer 2026 projects.

---
+++ src/data/projects-2026SU.js
@@ -0,0 +1,33 @@
+const projects2026Su = [
+    {
+      title: "600 Days of Figuring It All Out",
+      by: "Jamie Groom",
+      description: "A personal blog in the style of a retro web page that blends early internet aesthetics with modern web capabilities.",
+      url: "https://jamiemcadmagwdcapstone26.my.canva.site/",
+      img: "img/magwd/2026SU/groom.png",
+    },
+    {
+        by: "Heather Huston",
+        title: "Still-Light",
+        description: "A trauma-informed digital experience designed to support moments of overwhelm through presence, reflection, and gentle interaction.",
+        url: "https://heatherhdesign.github.io/Still-Light/",
+        img: "img/magwd/2026SU/huston.png",
+      },
+      {
+        by: "Andrew Rivera",
+        title: "Tiles",
+        description: "The entire pottery studio in your pocket.",
+        url: "https://arivera1452.github.io/capstone-website/",
+        img: "img/magwd/2026SU/rivera.png",
+      },
+      {
+        by: "Andrew Rossi",
+        title: "UI Organized",
+        description: "There is no reason to say no to design systems.",
+        url: "https://uiorganized.com/",
+        img: "img/magwd/2026SU/rossi.png",
+      },
+  ];
+  
+  export default projects2026Su;
+  
